a30a76ec8055fe7879e45c8d0265545991ed2e6e
Lukecicc <lukecicc@udel.edu>
2022-12-10 23:43:34 -0500

    Homework 6
    Homework 6 added size change and also audio to the balls

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // When the green ball collides with another ball, the size of the green ball changes
+        transform.localScale += new Vector3(3, 3, 3);    }
+}
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public AudioSource GigaChadAudio;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // When Red Ball collides with any other ball and audio named "GigaChad" plays which is popular on soical media like tiktok
+        GigaChadAudio.Play();
+    }
+}
+
